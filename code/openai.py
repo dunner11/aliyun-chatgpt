3ca8057f44761b872d835240b34744f8d7ae22fb
dunner11 <114581089+dunner11@users.noreply.github.com>
2023-02-17 15:58:00 +0800

    Update openai.py

--- code/openai.py
+++ code/openai.py
@@ -15,7 +15,7 @@ class OpenAi:
         # 替换为你的api_key <https://beta.openai.com/account/api-keys>
         self.keys = "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"
         # 这个是设置回答的长度,最大可以设置到4096 (免费额度为$18,该值影响你的用量)
-        self.max_tokens = 1024
+        self.max_tokens = 2048
         # 值越高意味着模型将承担更多风险。对于更具创造性的应用程序，请尝试 0.9,建议0.5-0.6
         self.temperature = 0.5
     def get_answer(self,prompt):
